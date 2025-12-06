c2b5d20f693f3978047a492dace3be177272582d
Mykola Novik <nik.novik48@gmail.com>
2025-12-06 13:30:20 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,17 +5,21 @@
  */
 function calculateRentalCost(days) {
   const dayCost = 40;
-  const fullPrice = dayCost * days;
+  const basePrice = dayCost * days;
+  const LONG_TERM = 7;
+  const MIDDLE_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const MIDDLE_TERM_DISCOUNT = 20;
 
-  if (days >= 7) {
-    return fullPrice - 50;
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return fullPrice - 20;
+  if (days >= MIDDLE_TERM) {
+    return basePrice - MIDDLE_TERM_DISCOUNT;
   }
 
-  return fullPrice;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
